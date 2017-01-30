6754b9a74994bb07e3ff29d728a614a67c0b2a4b
Daniel Stahl <danstahl1138@gmail.com>
2017-01-30 06:11:14 -0600

    split out npm commands and bash commands

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-node copyPackage
-npm install
-npm run build-electron-react
+
 sudo docker run --rm -ti -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder:wine /bin/bash -c "npm install && npm prune && npm run dist"
