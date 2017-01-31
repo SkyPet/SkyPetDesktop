bdcda849f3fff9b478c4c047f16c4566f884675d
Daniel Stahl <danstahl1138@gmail.com>
2017-01-31 16:09:12 -0600

    alpha-release

--- buildhtml.sh
+++ buildhtml.sh
@@ -5,8 +5,8 @@ npm run rm-public
 npm run rm-src
 npm run copy-src 
 npm run copy-public 
-REACT_APP_ELECTRON='true' node node_modules/react-scripts/scripts/build.js
-echo "$(</home/travis/build/SkyPet/SkyPetDesktop/npm-debug.log)"
+REACT_APP_ELECTRON='true' node ./node_modules/react-scripts/scripts/build.js
+#echo "$(</home/travis/build/SkyPet/SkyPetDesktop/npm-debug.log)"
 npm run post-process-react 
 npm run rename-client-build 
 npm run copy-icon
