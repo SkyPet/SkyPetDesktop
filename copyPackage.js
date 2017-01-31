110a61ecfe3849a37770d181d2a64ee15d305abe
Daniel Stahl <danstahl1138@gmail.com>
2017-01-31 14:30:32 -0600

    diagnostics for travis

--- copyPackage.js
+++ copyPackage.js
@@ -3,4 +3,6 @@ const htmlPackage=JSON.parse(fs.readFileSync("./SkyPetGUI/package.json"))
 var currPackage=JSON.parse(fs.readFileSync("./electron.package.json"))
 Object.assign(currPackage.dependencies, htmlPackage.dependencies);
 Object.assign(currPackage.scripts, htmlPackage.scripts);
+console.log(htmlPackage);
+console.log(currPackage);
 fs.writeFileSync("./package.json", JSON.stringify(currPackage));
