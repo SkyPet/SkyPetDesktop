1f289574126cb8f6384580e60ccceee61d0de77f
Daniel Stahl <danstahl1138@gmail.com>
2017-01-31 15:30:33 -0600

    one more attempt

--- buildhtml.sh
+++ buildhtml.sh
@@ -6,6 +6,7 @@ npm run rm-src
 npm run copy-src 
 npm run copy-public 
 cross-env REACT_APP_ELECTRON='true' npm run build 
+echo "$(</home/travis/build/SkyPet/SkyPetDesktop/npm-debug.log)"
 npm run post-process-react 
 npm run rename-client-build 
 npm run copy-icon
