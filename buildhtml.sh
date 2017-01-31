bc85f0b5408c3b99f039940c73e41067080546ee
Daniel Stahl <danstahl1138@gmail.com>
2017-01-31 15:55:09 -0600

    see if scripts work when called explicitly

--- buildhtml.sh
+++ buildhtml.sh
@@ -5,7 +5,7 @@ npm run rm-public
 npm run rm-src
 npm run copy-src 
 npm run copy-public 
-cross-env REACT_APP_ELECTRON='true' npm run build 
+REACT_APP_ELECTRON='true' node node_modules/react-scripts/scripts/build.js
 echo "$(</home/travis/build/SkyPet/SkyPetDesktop/npm-debug.log)"
 npm run post-process-react 
 npm run rename-client-build 
