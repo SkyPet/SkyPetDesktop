e7fd63776b0d00b42c7ca7b9723192b141ec0a15
Daniel Stahl <danstahl1138@gmail.com>
2017-01-31 15:08:58 -0600

    moved script away from npm

---
+++ buildhtml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -r -f build-react
+rm -r -f build 
+npm run rm-public 
+npm run rm-src
+npm run copy-src 
+npm run copy-public 
+cross-env REACT_APP_ELECTRON='true' npm run build 
+npm run post-process-react 
+npm run rename-client-build 
+npm run copy-icon
