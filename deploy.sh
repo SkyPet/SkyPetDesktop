acca0644d8a19930762aab4431893ec8140fab1b
Daniel Stahl <danstahl1138@gmail.com>
2017-02-04 09:07:09 -0600

    deployCommit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "Supply an npm version type, eg patch"
+    exit
+fi
 node copyPackage.js
 git add .
 git commit -m "deployCommit"
 npm version $1
 updatedVersion=$(node syncVersion.js);
-git add .
-git commit -m "release $updatedVersion"
-git tag -a $updatedVersion -m "updating to $updatedVersion"
-git push --follow-tags origin master
+echo "$updatedVersion"
+#git add .
+#git commit -m "release $updatedVersion"
+#git tag -a $updatedVersion -m "updating to $updatedVersion"
+#git push --follow-tags origin master
